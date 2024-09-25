aed405118d17c77581d75bc43304d583c67620bf
m jerin jacob <114149056+jerinjacobm@users.noreply.github.com>
2024-09-25 18:03:56 +0530

    bus object created

--- BusBooking/src/main/java/com/vast/co/Bus.java
+++ BusBooking/src/main/java/com/vast/co/Bus.java
@@ -1,5 +1,49 @@
 package com.vast.co;
 
 public class Bus {
+	private String bus_Number;
+	private String name;
+	private String departure;
+	private String arrival;
+
+	public Bus(String bus_Number, String name, String departure, String arrival) {
+		super();
+		this.bus_Number = bus_Number;
+		this.name = name;
+		this.departure = departure;
+		this.arrival = arrival;
+	}
+
+	public String getBus_Number() {
+		return bus_Number;
+	}
+
+	public void setBus_Number(String bus_Number) {
+		this.bus_Number = bus_Number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDeparture() {
+		return departure;
+	}
+
+	public void setDeparture(String departure) {
+		this.departure = departure;
+	}
+
+	public String getArrival() {
+		return arrival;
+	}
+
+	public void setArrival(String arrival) {
+		this.arrival = arrival;
+	}
 
 }
